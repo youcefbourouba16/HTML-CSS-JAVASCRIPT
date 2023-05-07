abea77d2fe1a06aa6bf39e81699ce3170fc5d3bd
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2023-05-07 20:09:43 +0100

    .

---
+++ cours/JavaScript/JS/monJs.js
@@ -0,0 +1,22 @@
+alert("wch rak ");
+function affich() {
+    alert("cv ");
+    var x=3.5;s="hmdlh";m=3;
+    alert("mon nom"+s);
+    
+}
+ function affich() {
+            alert("Alert");
+            
+        }
+        function confirm () {
+            confirm("Confirmation");
+            
+        }
+        function promptMoi() {
+            prompt("Search ? ");
+            
+        }
+        function imprimerMoi() {
+            window.print();
+        }
