b03e0ac3cf2f5a0fea2476a843de96120f90cc1d
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2023-05-15 22:55:35 +0100

    .

--- cours/test1.html/JS test/somme.js
+++ cours/test1.html/JS test/somme.js
@@ -1,3 +1,5 @@
+
+
 function somme(){
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
@@ -35,3 +37,4 @@ function div() {
 
 
 }
+
