242ed0fd14e23d862c0ef06db3aadbc52db3d3f8
youcefbourouba16 <109678299+youcefbourouba16@users.noreply.github.com>
2023-06-01 03:03:15 +0100

    .

---
+++ cours/test1.html/examen/scripts.js
@@ -0,0 +1,13 @@
+function changeBoxShadow() {
+    var element = document.getElementById("test");
+    element.style.boxShadow = "0 12px 15px 0 rgb(255, 0, 0), 0 17px 50px 0 rgba(0, 0, 0, 0.78)";
+}
+function openPopup() {
+    let v = document.getElementById("test");
+    v.classList.add("open-login-html")
+
+}
+function changeBoxShadow() {
+    var element = document.getElementById("test");
+    element.style.boxShadow = "0 12px 15px 0 rgb(0, 255, 40), 0 17px 50px 0 rgba(0, 0, 0, 0.78)";
+}
